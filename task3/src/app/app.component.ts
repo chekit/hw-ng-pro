ab786c3a9ff5b5003dad6430f48adfaf4b575cc8
Anton Cherepov <acherepov@solit-clouds.ru>
2018-06-22 13:14:02 +0300

    add content

--- task3/src/app/app.component.ts
+++ task3/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from './data';
 
 interface IData {
   [key: string]: string | number;
@@ -10,28 +11,11 @@ interface IData {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public data: IData[] = [
-    {
-      title: 'Post 1',
-      content: 'Post text',
-      published: 1529308222230
-    },
-    {
-      title: 'Post 2',
-      content: 'Post 2 text',
-      published: 1359240595003
-    },
-    {
-      title: 'Post 3',
-      content: 'Post 3 text',
-      published: 1529307397902
-    },
-    {
-      title: 'Post 4',
-      content: 'Post 4 text',
-      published: 1529240595993
-    }
-  ];
+  public posts: IData[] = data.slice(0, 4);
+
+  public ngOnInit(): void {
+    setTimeout(() => this.posts.push(...data.slice(4)), 3500);
+  }
 
   public trackByIndex(i: number): number {
     return i;
---
+++ task3/src/app/data.ts
@@ -0,0 +1,44 @@
+const data = [
+	{
+	  "title": "Post 1",
+	  "content": "Post text",
+	  "published": Date.now()
+	},
+	{
+	  "title": "Post 2",
+	  "content": "Post 2 text",
+	  "published": 1359240595003
+	},
+	{
+	  "title": "Post 3",
+	  "content": "Post 3 text",
+	  "published": 1529307397902
+	},
+	{
+	  "title": "Post 4",
+	  "content": "Post 4 text",
+	  "published": 1529240595993
+	},
+	{
+	  "title": "Post 5",
+	  "content": "Post 5 text",
+	  "published": 1529309222230
+	},
+	{
+	  "title": "Post 6",
+	  "content": "Post 6 text",
+	  "published": 1419240595003
+	},
+	{
+	  "title": "Post 7",
+	  "content": "Post 7 text",
+	  "published": 1129310397902
+	},
+	{
+	  "title": "Post 8",
+	  "content": "Post 8 text",
+	  "published": 1529240595993
+	}
+  ];
+  
+  export default data;
