74831b00286a4574469a830b9f2c8eb4d2c2fc74
Anton Cherepov <chekitdnb@gmail.com>
2022-11-24 00:03:04 +0100

    migrated to ng8

--- task1/src/app/utils/acl.directive.ts
+++ task1/src/app/utils/acl.directive.ts
@@ -1,41 +1,54 @@
-import { Directive, Input, OnDestroy, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import {
+  Directive,
+  Input,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { AuthService } from '../auth.service';
+import { AuthService } from "../auth.service";
 
 @Directive({
-  selector: '[acl]'
+  selector: "[acl]",
 })
 export class AclDirective implements OnInit, OnDestroy {
   private viewCreated = false;
-  private subscription: Subscription = new Subscription();
-  private currentRole$: Observable<string> = this.authService.getCurrentUserRole();
+  private currentRole$: Observable<string> =
+    this.authService.getCurrentUserRole();
 
-  @Input('acl') roles: string[] = [];
+  private destroy$: Subject<boolean> = new Subject();
+
+  @Input("acl") roles: string[] = [];
 
   constructor(
     private template: TemplateRef<any>,
     private viewContainer: ViewContainerRef,
     private authService: AuthService
-  ) { }
+  ) {}
 
   public ngOnInit(): void {
-    this.currentRole$.subscribe((role: string) => {
-      if (this.roles.some(r => r === role)) {
-        if (this.viewCreated) {
-          this.viewContainer.clear();
-        }
+    this.currentRole$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((role: string) => {
+        if (this.roles.some((r) => r === role)) {
+          if (this.viewCreated) {
+            this.viewContainer.clear();
+          }
 
-        this.viewContainer.createEmbeddedView(this.template);
-        this.viewCreated = true;
-      } else {
-        this.viewContainer.clear();
-        this.viewCreated = false;
-      }
-    });
+          this.viewContainer.createEmbeddedView(this.template);
+          this.viewCreated = true;
+        } else {
+          this.viewContainer.clear();
+          this.viewCreated = false;
+        }
+      });
   }
 
   public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
